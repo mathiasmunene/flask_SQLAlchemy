22a8c815dcbc629e710fb10d6bdb363e61ed9667
mathiasmunene <mathiasmunene@gmail.com>
2025-08-12 17:05:58 +0300

    routes added

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+
+app = Flask (__name__)
+
+@app.route('/')
+def index():
+    return '<p>Hello World</p>'
+
+@app.route('/courses')
+def courses():
+    return f'This is the courses page'
+
+@app.route('/courses/<int:course_id>')
+def courses_id(course_id):
+    return f'This course id is: {course_id}'
+
+@app.route('/courses/<course_name>')
+def courses_name(course_name):
+    return f'This course name is: {course_name}'
+
+@app.route('/courses')
+def couses_detail_query():
+    c_name=request.arg.get('name')
+    c_date=request.arg.get('date')
+    return f'The course name is {c_name} created on {c_date}'
+
+@app.route('/about')
+def about():
+    return "About"
+
+@app.route('/contact')
+def contact():
+    return "contact"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port = 3000)
