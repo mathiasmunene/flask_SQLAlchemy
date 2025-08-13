f0ed9c6f6abeba3b2bd50fb7e988b057045b77a3
mathiasmunene <mathiasmunene@gmail.com>
2025-08-13 15:08:29 +0300

    syntax corrected

--- models.py
+++ models.py
@@ -5,15 +5,19 @@ db = SQLAlchemy()
 class Students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(200), nullable=False)
-    age = db.colum(db.integer, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    enrollments = db.relationship("CourseEnrollment", back_populates="student", cascade="all, delete-orphan")
 
 class Courses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    price = db.colum(db.integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    enrollments = db.relationship("CourseEnrollment", back_populates="course", cascade="all, delete-orphan")
 
 class CourseEnrollment(db.Model):
-    id = db.columm(db.integer, primary_key=True)
-    course_id = db.column(db.Integer(), db.ForeignKey("courses.id"), nullable=False)
-    student_name = db.column(db.Integer(), db.ForeignKey("students_id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.id"), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    course = db.relationship("Courses", back_populates="enrollments")
+    student = db.relationship("Students", back_populates="enrollments")
 
